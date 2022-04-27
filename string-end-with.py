bfe963e175ce3de5524178450d5b058ace387a4e
Carlos Caicedo <calicheccmusica@gmail.com>
2022-04-27 15:48:56 -0700

    Algorithm practice

---
+++ string-end-with.py
@@ -0,0 +1,13 @@
+# STRING ENDS WITH 
+
+# Complete the solution so that it returns true if the first argument(string) 
+# passed in ends with the 2nd argument (also a string).
+
+def stringEndsWith(string, ending):
+    return string.endswith(ending)
+
+print(stringEndsWith('Tacoma', 'oma'))
+print(stringEndsWith('Argument', 'ment'))
+print(stringEndsWith('Balance', 'once'))
+print(stringEndsWith('Chicago', 'ego'))
+
